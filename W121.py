b0d550c936b2af532f4886efac1cd2031c637c40
Gina Gearing <np009181@live.reading.ac.uk>
2017-10-02 14:30:32 +0100

    First commit

---
+++ W121.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+#Question 1#
+N=int(1e7)
+a=0
+for i in range(N):
+    a+=0.01
+
+print(a)
+
+#10e4 works, however 10e7 does not give the right answer as the mantissa#
+#\is not long enough#
+
---
+++ W122.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 14:58:33 2017
+
+@author: np009181
+"""
+#Question 2#
+
+def intef(f, a, b, n):
+    #integrate function f by Gaussian quadrature#
+    #here n is the number of intervals#
+#Define function f#
+    f=3*x-2
+#Set integration limits and number of integrals#
+a=0
+b=10
+n=3
+#Calculate the integral length#
+dx=(b-a)/n
+#Initialise the integral#
+I=0.0
+
+for i in xrange(0,n):
+    
+    I+=(a + (i+0.5)*dx)
+    
+I*=dx
+return I
