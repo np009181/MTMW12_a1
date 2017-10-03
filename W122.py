fa84e423d33ba151720cafddf8dfa1b224492a6a
Gina Gearing <np009181@live.reading.ac.uk>
2017-10-03 15:10:21 +0100

    Improved code readability

--- W122.py
+++ W122.py
@@ -10,19 +10,19 @@ def intef(f, a, b, n):
     #integrate function f by Gaussian quadrature#
     #here n is the number of intervals#
 #Define function f#
-    f=3*x-2
+    f = 3*x - 2
 #Set integration limits and number of integrals#
-a=0
-b=10
-n=3
+a = 0
+b = 10
+n = 3
 #Calculate the integral length#
-dx=(b-a)/n
+dx = (b-a)/n
 #Initialise the integral#
-I=0.0
+I = 0.0
 
 for i in xrange(0,n): #use this to find the sum up to n#
     
-    I+=(a + (i+0.5)*dx)
+    I += (a + (i+0.5)*dx)
     
-I*=dx
+I* = dx
 return I
